42c51459caa6934ca558daeba26ede5456704fbc
Severus-Yang <boyu.yang258@gmail.com>
2026-08-28 21:19:08 -0400

    feat: default download source to GitHub instead of Mirror

--- _src/MFAAvalonia/Helper/Instances.cs
+++ _src/MFAAvalonia/Helper/Instances.cs
@@ -651,7 +651,7 @@ public static partial class Instances
                 if (IsResolved<VersionUpdateSettingsUserControlModel>())
                 {
                     var version = VersionUpdateSettingsUserControlModel;
-                    version.DownloadSourceIndex = ConfigurationManager.Current.GetValue(ConfigurationKeys.DownloadSourceIndex, 1);
+                    version.DownloadSourceIndex = ConfigurationManager.Current.GetValue(ConfigurationKeys.DownloadSourceIndex, 0);
                     version.UIUpdateChannelIndex = ConfigurationManager.Current.GetValue(ConfigurationKeys.UIUpdateChannelIndex, 2);
                     version.ResourceUpdateChannelIndex = ConfigurationManager.Current.GetValue(ConfigurationKeys.ResourceUpdateChannelIndex, 2);
                     version.GitHubToken = SimpleEncryptionHelper.Decrypt(ConfigurationManager.Current.GetValue(ConfigurationKeys.GitHubToken, string.Empty));
